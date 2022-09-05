e5bd330354d3aff71bf3982efa556f673565d678
Ernest.Kozhokmatov <ernestea@icloud.com>
2022-09-05 13:33:43 -0500

    mvc path parameter mapping

--- Spring-08-Spring-MVC-Model/src/main/java/com/codefoe/controller/CarController.java
+++ Spring-08-Spring-MVC-Model/src/main/java/com/codefoe/controller/CarController.java
@@ -2,6 +2,7 @@ package com.codefoe.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -16,6 +17,21 @@ public class CarController {
             @RequestParam(required = false,defaultValue = "Model 3") String carModel,
             @RequestParam(required = false,defaultValue = "2020") String year,
             Model model
+            )
+    {
+        model.addAttribute("make",make);
+        model.addAttribute("carModel",carModel);
+        model.addAttribute("year",year);
+        return "car/car-info";
+    }
+
+    @RequestMapping("/info2/{make}/{carModel}/{year}")//localhost:8088/car/info?make=Honda
+    public String carInfo2
+            (
+                    @PathVariable String make,
+                    @PathVariable String carModel,
+                    @PathVariable String year,
+                    Model model
             )
     {
         model.addAttribute("make",make);
